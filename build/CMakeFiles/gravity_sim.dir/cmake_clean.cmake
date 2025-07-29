0f88ff3728ac3112fa44f7220ed2fcf88b980dd2
sam uribe <samuribe@sams-MacBook-Air-2.local>
2025-07-29 15:39:50 -0700

    got opengl set up only took 2 hours lol

---
+++ build/CMakeFiles/gravity_sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gravity_sim.dir/src/glad.c.o"
+  "CMakeFiles/gravity_sim.dir/src/glad.c.o.d"
+  "CMakeFiles/gravity_sim.dir/src/main.cpp.o"
+  "CMakeFiles/gravity_sim.dir/src/main.cpp.o.d"
+  "gravity_sim"
+  "gravity_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/gravity_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
